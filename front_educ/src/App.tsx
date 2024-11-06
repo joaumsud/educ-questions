d90272c46bffed9ca7bc2452f5ce1826d51dec44
João Paulo de Lima Silva <joaumsud@hotmail.com>
2024-11-05 22:42:00 -0300

    Melhorando as importações do carrossel e o css do navbar

--- front_educ/src/App.tsx
+++ front_educ/src/App.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
-import Carousel from './components/Carousel/Carousel';
+
 
 const  App: React.FC = ()=> {
 
-  const images = [
-      'img/professor.png',
-      'img/professor01.png',
-      'img/professora.jpg',
-  ];
   return (
     <>
       <Navbar/>
       <Home/>
-      <Carousel images={images} />
+      
     </>
   );
 }
--- front_educ/src/pages/Home.tsx
+++ front_educ/src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Carousel from '../components/Carousel/Carousel';
 
 const Home: React.FC = ()=>{
+    const images = [
+        'img/professor.png',
+        'img/professor01.png',
+        'img/professora.jpg',
+    ];
     return(
         <>
-        <h1>Bem-vindo ao Quest Fácil!</h1>
+        <h1 style={{ paddingTop: 20 }}>Bem-vindo ao Quest Fácil!</h1>
+        <Carousel images={images} />
         </>
     );
 };
